dacd86ae3fbe521f3074582132c8ba5a68667e76
Artem <200201@student.madi.ru>
2022-10-17 14:36:18 +0300

    Add NotoSans fonts

--- MobileAppSchedule/AssemblyInfo.cs
+++ MobileAppSchedule/AssemblyInfo.cs
@@ -6,3 +6,7 @@ using Xamarin.Forms.Xaml;
 [assembly: ExportFont("fa-brands-400.ttf", Alias = "FAB")]
 [assembly: ExportFont("fa-regular-400.ttf", Alias = "FAR")]
 [assembly: ExportFont("fa-solid-900.ttf", Alias = "FAS")]
+[assembly: ExportFont("NotoSans-Bold.ttf", Alias = "NSB")]
+[assembly: ExportFont("NotoSans-BoldItalic.ttf", Alias = "NSBI")]
+[assembly: ExportFont("NotoSans-Italic.ttf", Alias = "NSI")]
+[assembly: ExportFont("NotoSans-Regular.ttf", Alias = "NSR")]
