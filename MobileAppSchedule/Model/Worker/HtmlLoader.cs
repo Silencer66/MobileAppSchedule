9f105b3a680b390fdf43d08dc2aabe4c7ae9853e
Artem <200201@student.madi.ru>
2022-10-29 01:49:04 +0300

    Add HtmlLoader & ParserWorker

---
+++ MobileAppSchedule/Model/Worker/HtmlLoader.cs
@@ -0,0 +1,43 @@
+﻿using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using MobileAppSchedule.Model.Parser;
+
+namespace MobileAppSchedule.Model.Worker
+{
+    class HtmlLoader
+    {
+        readonly HttpClient client;
+        readonly string url;
+        readonly string contentType;
+
+        public HtmlLoader(IParserSettings settings)
+        {
+            client = new HttpClient();
+            url = settings.BaseUrl;
+            contentType = settings.ContentType;
+        }
+
+        public async Task<string> GetScheduleByGroupName(string payload)
+        {
+
+            using (var request = new HttpRequestMessage(HttpMethod.Post, url))
+            {
+                HttpContent content = new StringContent(payload,
+                    Encoding.UTF8,
+                    contentType
+                );
+                request.Content = content;
+
+                var response = await client.SendAsync(request);
+                if (response == null || response.StatusCode != HttpStatusCode.OK)
+                {
+                    return "Ответ пустой";
+                }
+                var responseBody = await response.Content.ReadAsStringAsync();
+                return responseBody;
+            }
+        }
+    }
+}
---
+++ MobileAppSchedule/Model/Worker/ParserWorker.cs
@@ -0,0 +1,81 @@
+﻿using AngleSharp.Html.Parser;
+using MobileAppSchedule.Model.Parser;
+using System;
+using System.Threading.Tasks;
+
+namespace MobileAppSchedule.Model.Worker
+{
+    class ParserWorker<T> where T : class
+    {
+        private bool _isLoad;
+        IParser<T> parser;
+        IParserSettings parserSettings;
+        HtmlLoader loader;
+
+        #region Properties
+        public IParser<T> Parser
+        {
+            get => parser;
+            set => parser = value;
+        }
+
+        public IParserSettings ParserSettings
+        {
+            get => parserSettings;
+            set
+            {
+                parserSettings = value;
+                loader = new HtmlLoader(value);
+            }
+        }
+        #endregion
+
+        #region Events
+        /// <summary> информирует о получении данных после парсинга распписания </summary>
+        public event Action<object, T> OnGroupSchedule;
+        /// <summary> Информирование при завершении работы парсера </summary>
+        public event Action<object> OnComplete;
+        #endregion
+
+        #region Constructors
+        public ParserWorker(IParser<T> parser)
+        {
+            this.parser = parser;
+        }
+
+        //this(parser) - вызывает первый конструктор
+        public ParserWorker(IParser<T> parser, IParserSettings parserSettings) : this(parser)
+        {
+            this.parserSettings = parserSettings;
+            loader = new HtmlLoader(parserSettings);
+        }
+
+        #endregion
+
+        public async Task LoadDataByGroupName()
+        {
+            _isLoad = true;
+            await Worker();
+        }
+
+        public async Task Worker()
+        {
+            var source = await loader.GetScheduleByGroupName(parserSettings.GroupNames[124]);
+            if (source == "Ответ пустой")
+            {
+                OnComplete?.Invoke("Ответ пустой");
+                _isLoad = false;
+                return;
+            }
+
+            var domParser = new HtmlParser();
+            var document = await domParser.ParseDocumentAsync(source);
+
+            var result = parser.Parse(document); //вернётся объект типа Schedule
+
+            _isLoad = false;
+            OnGroupSchedule?.Invoke(this, result);
+            OnComplete?.Invoke("Ответ не пустой");
+        }
+    }
+}
