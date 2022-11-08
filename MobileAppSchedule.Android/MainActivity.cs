c141923b535eae0a2b58739e9f7b4c4d3dbe6de7
Artem <200201@student.madi.ru>
2022-11-08 23:37:33 +0300

    Change icons

--- MobileAppSchedule.Android/MainActivity.cs
+++ MobileAppSchedule.Android/MainActivity.cs
@@ -7,7 +7,7 @@ using Android.OS;
 
 namespace MobileAppSchedule.Droid
 {
-    [Activity(Label = "MobileAppSchedule", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
+    [Activity(Label = "Расписание", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
