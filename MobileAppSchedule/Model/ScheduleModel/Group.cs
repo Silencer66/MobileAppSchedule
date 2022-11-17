7d6b0193cf3f19c97d8173e77c2f08f2b2924015
Artem <200201@student.madi.ru>
2022-11-18 00:11:50 +0300

    Added a group class for list of groupnames

---
+++ MobileAppSchedule/Model/ScheduleModel/Group.cs
@@ -0,0 +1,14 @@
+﻿namespace MobileAppSchedule.Model.ScheduleModel
+{
+    internal class Group
+    {
+        public string GroupName { get; set; }
+        public bool IsFavorite { get; set; }
+
+        public Group(string groupName, bool isFavorite)
+        {
+            GroupName = groupName;
+            IsFavorite = isFavorite;
+        }
+    }
+}
