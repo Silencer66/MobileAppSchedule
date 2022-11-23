c9efb2013ad98dd7bddec883f807649f07061e28
Artem <200201@student.madi.ru>
2022-11-23 14:58:45 +0300

    create mycollection for list of disciplines

---
+++ MobileAppSchedule/Model/MyObservableCollection.cs
@@ -0,0 +1,20 @@
+﻿using MvvmHelpers;
+using System.Linq;
+
+namespace MobileAppSchedule.Model
+{
+    internal class MyObservableCollection<DayOfWeek> : ObservableRangeCollection<DayOfWeek>
+    {   
+        public DayOfWeek this[DayOfWeek day]
+        {
+            get
+            {
+                return this.Where(a => a.Equals(day)).FirstOrDefault();
+            }
+            set
+            {
+                this[day] = value;
+            }
+        }
+    }
+}
